5025c76dca6608825a5193b1a4944c0489947497
Chris Kihlstrom <ckihlst@gmail.com>
2017-11-06 23:05:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -11,7 +11,9 @@ def won?(board)
   elsif board[bottom_row_win[6]] == "X" && board[bottom_row_win[7]] == && board [bottom_row_win[8]] = "X"
     "X won the bottom row"
   elsif board[left_row_win[0]] == "X" && board[left_row_win[3]] == && board [left_row_win[6]] = "X"
-    "X won the bottom row"
+    "X won the Left row"
+  elsif board[center_row_win[0]] == "X" && board[center_row_win[3]] == && board [center_row_win[6]] = "X"
+    "X won the center row"
   end
 end
 # Define your WIN_COMBINATIONS constant
@@ -20,7 +22,7 @@ WIN_COMBINATIONS = [
   [3,4,5], #Middle row
   [6,7,8], #Bottom row
   [0,3,6], #Left row
-  [1,4,7], #Vertical middle row
+  [1,4,7], #center row
   [2,5,8], #Right row
   [0,4,8], #diagonal top left
   [2,4,6]  #diagonal top right
